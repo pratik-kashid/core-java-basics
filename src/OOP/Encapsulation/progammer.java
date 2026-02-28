4bce46a4366ad64c1c93421a9591be1ffa98b38c
pratik <kashidpratik100@gmail.com>
2026-02-28 08:43:57 +0530

    Initial commit

--- src/OOP/Encapsulation/progammer.java
+++ src/OOP/Encapsulation/progammer.java
@@ -1,10 +1,38 @@
 package OOP.Encapsulation;
 
-class encapsulation {
+//Encapsulation
 
+class Person {
+    //Private data
+    private String name;
+    private int age;
+
+    //getter method
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    //setter method
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
 public class progammer {
+    static void main() {
+        Person person = new Person();
+        person.setName("Pratik");
+        person.setAge(20);
 
-
+        System.out.println("Name: "+person.getName());
+        System.out.println("Age: "+person.getAge());
+    }
 }

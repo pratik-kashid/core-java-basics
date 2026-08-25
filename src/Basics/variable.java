a5ca5708507c282ade0036acae0195554d2a8bf2
Pratik Kashid <kashidpratik100@gmail.com>
2026-08-25 17:33:01 +0530

    Added Wrapper classes

---
+++ src/Basics/variable.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class variable {
+    static void main(String[] args) {
+
+        // Integer Variable
+        int age = 19;
+
+        // String Variable
+        String name = "Pratik";
+
+        System.out.print("Hey " + name + ", you are " + age + " years old.");
+    }
+}
---
+++ src/Basics/wrapperClasses.java
@@ -0,0 +1,41 @@
+package Basics;
+
+public class wrapperClasses {
+    static void main(String[] args) {
+        byte b = 1;
+        Byte byteObj = Byte.valueOf(b);
+
+        int i = 10;
+        Integer intObj = Integer.valueOf(i);
+
+        float f = 18.6f;
+        Float floatObj = Float.valueOf(f);
+
+        double d = 250.5;
+        Double doubleObj = Double.valueOf(d);
+
+        char c = 'a';
+        Character charObj = c; // autoboxing
+
+        System.out.println("Wrapper Objects:");
+        System.out.println(byteObj);
+        System.out.println(intObj);
+        System.out.println(floatObj);
+        System.out.println(doubleObj);
+        System.out.println(charObj);
+
+        // Unboxing
+        byte bv = byteObj;
+        int iv = intObj;
+        float fv = floatObj;
+        double dv = doubleObj;
+        char cv = charObj;
+
+        System.out.println("\nUnwrapped values:");
+        System.out.println(bv);
+        System.out.println(iv);
+        System.out.println(fv);
+        System.out.println(dv);
+        System.out.println(cv);
+    }
+}

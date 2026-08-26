c997b324946f2bf94e4154be79ce9da7aa22ec71
Pratik Kashid <kashidpratik100@gmail.com>
2026-08-26 12:29:59 +0530

    Study about variables

--- src/Basics/variable.java
+++ src/Basics/variable.java
@@ -4,6 +4,7 @@ public class variable {
     static void main(String[] args) {
 
         // Integer Variable
+        // age is variable identifier
         int age = 19;
 
         // String Variable
